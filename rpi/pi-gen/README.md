ba3f3b03a302af9758d9634643885526e43a7a68
Matthew Bowman <mgb@otr.mx>
2020-11-05 19:02:09 +0100

    WIP

--- rpi/pi-gen/README.md
+++ rpi/pi-gen/README.md
@@ -5,7 +5,7 @@ A collection of my custom builds of Raspbian OS using (link to ../../docker/pi-g
 ```
 > git clone https://github.com/mgbowman/homelab.git
 > cd homelab/rpi/pi-gen
-> ./build.sh configs/ansible-lite
+> ./build.sh ./configs/ansible-lite
 
 # Baked images will be in deploy/
 > ls -lah deploy/
--- rpi/pi-gen/build.sh
+++ rpi/pi-gen/build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -eu
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CONFIG="$( cd "$( dirname "$1" )" >/dev/null 2>&1 && pwd )/$( basename "$1" )"
+IMG_NAME="$( basename "${CONFIG}" )"
+
+# > ./build.sh ./configs/ansible-lite
+
 docker run --rm --privileged \
-  -e IMG_NAME="$(basename $1)" \
-  -e $PWD/defaults \
-  -e $PWD/$1 \
-  -v $PWD/$1:/pi-gen/config:ro \
-  -v $PWD/stages:/pi-gen/stages:ro \
-  -v $PWD/deploy:/pi-gen/deploy \
+  -e IMG_NAME="${IMG_NAME}" \
+	-e TARGET_HOSTNAME="pi" \
+	-e TIMEZONE_DEFAULT="Europe/Amsterdam" \
+	-e LOCALE_DEFAULT="en_US.UTF-8" \
+	-e KEYBOARD_MAP="us" \
+	-e KEYBOARD_LAYOUT="English (US)" \
+	-e DEPLOY_ZIP=0 \
+  -e $CONFIG \
+  -v $DIR/stages:/pi-gen/stages:ro \
+  -v $DIR/deploy:/pi-gen/deploy \
   mgbowman/pi-gen
